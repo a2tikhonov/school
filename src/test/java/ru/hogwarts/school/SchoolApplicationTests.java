52b58771faa0705c3344d6bb58d2c23c9f1e535d
a2tikhonov <gub!St@d1um@rc@d1um>
2022-09-05 13:42:07 +0300

    Домашка по тестам

--- src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
+++ src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
@@ -1,16 +1,23 @@
 package ru.hogwarts.school;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import ru.hogwarts.school.controller.StudentController;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SchoolApplicationTests {
@@ -25,16 +32,16 @@ class SchoolApplicationTests {
     private TestRestTemplate testRestTemplate;
     @Test
     void contextLoads() throws Exception {
-        Assertions.assertThat(studentController).isNotNull();
+        assertThat(studentController).isNotNull();
     }
 
     @Test
     void createStudent() {
         Student student = new Student(1L, "Alex", 18);
         Faculty faculty = new Faculty(1L, "faculty1", "red");
-        Assertions.assertThat(this.testRestTemplate.postForObject("http://localhost:" + port + "/faculty", faculty, Faculty.class)).isEqualTo(faculty);
+        assertThat(this.testRestTemplate.postForObject("http://localhost:" + port + "/faculty", faculty, Faculty.class)).isEqualTo(faculty);
         student.setFaculty(faculty);
-        Assertions.assertThat(this.testRestTemplate.postForObject("http://localhost:" + port + "/student", student, Student.class)).isEqualTo(student);
+        assertThat(this.testRestTemplate.postForObject("http://localhost:" + port + "/student", student, Student.class)).isEqualTo(student);
         this.testRestTemplate.delete("http://localhost:" + port + "/student/1");
         this.testRestTemplate.delete("http://localhost:" + port + "/faculty/1");
     }
@@ -49,7 +56,7 @@ class SchoolApplicationTests {
         Student student1 = new Student(null, "Oleg", 20);
         student1.setFaculty(faculty);
         this.testRestTemplate.postForObject("http://localhost:" + port + "/student", student1, Student.class);
-        Assertions.assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/student", String.class ))
+        assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/student", String.class ))
                 .contains(Arrays.asList("Alex", "Oleg", "faculty1", "red"));
     }
 
@@ -63,7 +70,7 @@ class SchoolApplicationTests {
         this.testRestTemplate.postForObject("http://localhost:" + port + "/faculty", faculty, Faculty.class);
         this.testRestTemplate.postForObject("http://localhost:" + port + "/student", student, Student.class);
         this.testRestTemplate.postForObject("http://localhost:" + port + "/student", student1, Student.class);
-        Assertions.assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/student/2", Student.class ))
+        assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/student/2", Student.class ))
                 .isEqualTo(student1);
     }
 
@@ -77,7 +84,7 @@ class SchoolApplicationTests {
         Student student1 = new Student(2L, "Oleg", 20);
         student1.setFaculty(faculty);
         this.testRestTemplate.postForObject("http://localhost:" + port + "/student", student1, Student.class);
-        Assertions.assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/student/age?age=20", String.class))
+        assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/student/age?age=20", String.class))
                 .contains(Arrays.asList("Oleg"));
     }
 
@@ -93,7 +100,7 @@ class SchoolApplicationTests {
         Student student1 = new Student(2L, "Oleg", 20);
         student1.setFaculty(faculty1);
         this.testRestTemplate.postForObject("http://localhost:" + port + "/student", student1, Student.class);
-        Assertions.assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/student/2/faculty", String.class))
+        assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/student/2/faculty", String.class))
                 .contains(Arrays.asList("faculty2", "green"));
     }
 
@@ -106,7 +113,7 @@ class SchoolApplicationTests {
         this.testRestTemplate.postForObject("http://localhost:" + port + "/student", student, Student.class);
         student.setName("Oleg");
         this.testRestTemplate.put("http://localhost:" + port + "/student", student, Student.class);
-        Assertions.assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/student/1", String.class))
+        assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/student/1", String.class))
                 .contains(Arrays.asList("Oleg"));
     }
 
@@ -121,7 +128,7 @@ class SchoolApplicationTests {
         this.testRestTemplate.postForObject("http://localhost:" + port + "/student", student, Student.class);
         this.testRestTemplate.postForObject("http://localhost:" + port + "/student", student1, Student.class);
         this.testRestTemplate.delete("http://localhost:" + port + "/student/1", Student.class);
-        Assertions.assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/student/1", Student.class))
+        assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/student/1", Student.class))
                 .isNull();
     }
 
